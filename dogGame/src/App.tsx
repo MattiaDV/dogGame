d037d6b1b37bdaa38043390127061e4343395939
ifthennextagency <ifthen.next.agency@gmail.com>
2026-04-25 10:52:36 +0200

    Update all

--- dogGame/src/App.tsx
+++ dogGame/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <h1>Ciao mondo</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Ciao mondo</h1>} />
+      <Route path="/test" element={<h1>Ciao test</h1>} />
+      <Route path="*" element={<h1>Error 404</h1>} />
+    </Routes>
   )
 }
 
