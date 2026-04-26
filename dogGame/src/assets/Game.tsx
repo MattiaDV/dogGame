2ea0dd8ed2a11371af1e49fa6627750f7e4b7fa8
ifthennextagency <ifthen.next.agency@gmail.com>
2026-04-26 17:15:48 +0200

    Update all

--- dogGame/src/assets/Game.tsx
+++ dogGame/src/assets/Game.tsx
@@ -14,6 +14,22 @@ export function Game() {
     const screenHeight = window.innerHeight - 60;
     const [cloud, setCloud] = useState<number[]>([]);
 
+    fetch("7.netlify/functions/data", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            event: "dog_spawn",
+            client: {
+                userAgent: navigator.userAgent,
+                language: navigator.language,
+                platform: navigator.platform,
+                width: window.innerWidth,
+                height: window.innerHeight,
+                time: new Date().toISOString()
+            }
+        })
+    })
+
     useEffect(() => {
         let arr = [];
         for (let i = 0; i < 4; i++) {
--- dogGame/src/assets/cagnolino.tsx
+++ dogGame/src/assets/cagnolino.tsx
@@ -6,21 +6,6 @@ type Props = {
 }
 
 export function CreateDog({x, y}: Props) {
-    fetch("7.netlify/functions/data", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            event: "dog_spawn",
-            client: {
-                userAgent: navigator.userAgent,
-                language: navigator.language,
-                platform: navigator.platform,
-                width: window.innerWidth,
-                height: window.innerHeight,
-                time: new Date().toISOString()
-            }
-        })
-    })
     return (
         <>
             <img 
